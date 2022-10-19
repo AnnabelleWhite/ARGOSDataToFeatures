a7fcb648ac034d2cb4e7b98c85fdadc13e7efa41
AnnabelleWhite <112416887+AnnabelleWhite@users.noreply.github.com>
2022-10-19 15:02:36 -0400

    Create ImportARGOS.py

---
+++ ImportARGOS.py
@@ -0,0 +1,11 @@
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: October 2022
+## Author: Annabelle.White@duke.edu (for ENV859)
+##---------------------------------------------------------------------
